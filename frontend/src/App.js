90fdaf334bc9d96b590465867bf8af9a8031ff82
Mari Cruz <229509355+marisol-source@users.noreply.github.com>
2026-05-04 01:07:42 -0500

    fix api productos for production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [carrito, setCarrito] = useState([]);
 
   useEffect(() => {
-    fetch("https://ecommerce-platform-gw0x.onrender.com")
+    fetch("https://ecommerce-platform-gw0x.onrender.com/productos")
       .then(res => res.json())
       .then(data => setProductos(data));
   }, []);
